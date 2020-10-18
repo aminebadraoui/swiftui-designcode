450d49a2fbf7a477e96db6c40a1b91c8cf6c00b2
Amine Badraoui <aminebadraoui.contact@gmail.com>
2020-10-17 21:40:22 -0400

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Amine on 2020-10-17.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
