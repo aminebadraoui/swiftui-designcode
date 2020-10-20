457243fb75723b9fe8156f6b4913f9efe8d8b0f6
Amine Badraoui <aminebadraoui.contact@gmail.com>
2020-10-20 08:11:28 -0400

    tutorial 1

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            BackCardView()
+                .offset(x: 0, y: -40)
+            
+            BackCardView()
+                .offset(x: 0, y: -20)
+            
+            CardView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 20)
+            Spacer()
+            Image("Card1")
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 300.0, height: 220.0)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    
     }
 }
